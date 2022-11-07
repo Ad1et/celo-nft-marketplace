40589c92e226bf3a73c1c47842b8733199d2359d
Ad1et <adlet.shotpaev@gmail.com>
2022-11-07 09:04:50 +0600

    first commit

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/ad1et/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0x52A8a9eA7D8018E2Be31Fac67a2Acdf46cFC9780";
